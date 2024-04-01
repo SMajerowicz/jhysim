e3bd0890b9961a50c3e9b001fe432c38dd04dc8d
SMajerowicz <majerow@yahoo.fr>
2024-04-01 21:36:55 +0200

    Initialize some easy classes

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # JHySim
  Java Hydrodynamics Simulations
+ 
+## What is it? 
+ Here is a software to perform some easily hydrodynamics simulations in Java
+ 
+## Plug a new simulation
+* reach for the xml file path name
+* check if the .class file really exists
+* if this is the case, put the new simulation in the "plugged_simulations.xml" XML file
 
- 	Here is a software to perform some hydrodynamics simulations in Java
+* update the simulations.xml file
+* create a .class file of your simulation class
+* create a your_simulation.xml file where are listed the profiles and variables which are needed to initialize your new simulation (the Spatial Resolution double variable has to be the first of the double variable in the XML file)
---
+++ src/main/java/org/jhysim/gui/JXmlFileChooser.java
@@ -0,0 +1,32 @@
+package org.jhysim.gui;
+
+import javax.swing.JFileChooser;
+
+import org.jhysim.gui.filechooser.XmlFileFilter;
+
+/**
+ * To only choose an Xml file
+ * @author Sébastien Majerowicz
+ */
+public class JXmlFileChooser extends JFileChooser
+{
+	private static final long serialVersionUID = 3040123414504651970L;
+
+/**
+ * Constructor
+ * @param currentDirectory String current directory
+ * @param dialogTitle String dialog title
+ */
+	public JXmlFileChooser (String currentDirectory, String dialogTitle)
+	{
+		super(currentDirectory);
+		this.setDialogTitle(dialogTitle);
+
+//clear the obvious file filter
+		this.removeChoosableFileFilter(this.getAcceptAllFileFilter());
+//only files can be selected
+		this.setFileSelectionMode(JFileChooser.FILES_ONLY);
+//add file filters
+		this.addChoosableFileFilter(new XmlFileFilter());
+	}
+}
